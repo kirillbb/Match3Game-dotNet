e0f9e601d04cd04741b4cacd979ef2e2495665af
Kirill Sokolov <kirill.sokolov.work@gmail.com>
2022-10-26 10:30:22 +0300

    Analog  3-in-a-row game is working now

---
+++ Drawer.cs
@@ -0,0 +1,65 @@
+﻿using TestMatch3.Interfaces;
+
+namespace TestMatch3
+{
+    public class Drawer : IDrawer
+    {
+        public void Draw(string[,] map, int size)
+        {
+            for (int x = 0; x < size; x++)
+            {
+                for (int y = 0; y < size; y++)
+                {
+                    switch (map[x, y])
+                    {
+                        case "0":
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            break;
+                        case "1":
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            break;
+                        case "2":
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            break;
+                        case "3":
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            break;
+                    }
+
+                    Console.Write($"({map[x, y]})");
+                    Console.ResetColor();
+                    Console.Write(" ");
+                }
+
+                Console.WriteLine("\n");
+            }
+        }
+
+        public void NewLine()
+        {
+            Console.WriteLine("------------------------------------");
+        }
+
+        public void Message(int score)
+        {
+            NewLine();
+            Console.WriteLine($"To continue press Enter. Current Score: {score}");
+            NewLine();
+        }
+
+        public void GameOver(int totalScore)
+        {
+            string gameOver =
+                "░██████╗░░█████╗░███╗░░░███╗███████╗░░░░░█████╗░██╗░░░██╗███████╗██████╗░██╗\r\n" +
+                "██╔════╝░██╔══██╗████╗░████║██╔════╝░░░░██╔══██╗██║░░░██║██╔════╝██╔══██╗██║\r\n" +
+                "██║░░██╗░███████║██╔████╔██║█████╗░░░░░░██║░░██║╚██╗░██╔╝█████╗░░██████╔╝██║\r\n" +
+                "██║░░╚██╗██╔══██║██║╚██╔╝██║██╔══╝░░░░░░██║░░██║░╚████╔╝░██╔══╝░░██╔══██╗╚═╝\r\n" +
+                "╚██████╔╝██║░░██║██║░╚═╝░██║███████╗░░░░╚█████╔╝░░╚██╔╝░░███████╗██║░░██║██╗\r\n" +
+                "░╚═════╝░╚═╝░░╚═╝╚═╝░░░░░╚═╝╚══════╝░░░░░╚════╝░░░░╚═╝░░░╚══════╝╚═╝░░╚═╝╚═╝";
+            NewLine();
+            Console.WriteLine(gameOver);
+            Console.WriteLine($"Your score is: {totalScore}");
+            NewLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using TestMatch3.Interfaces;
+
+namespace TestMatch3
+{
+    public static class Program
+    {
+        private static void Main(string[] args)
+        {
+            IDrawer drawer = new Drawer();
+            Match3Game starter = new Match3Game(drawer);
+            starter.Run();
+            Console.ReadLine();
+        }
+    }
+}
